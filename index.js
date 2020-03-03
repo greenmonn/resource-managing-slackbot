9e096cc69a6eb0309c043a0490631d26a5dc62bf
greenmon <greenmon@kaist.ac.kr>
2020-03-03 18:10:48 +0900

    Add commands for release, check, find free nodes

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ app.listen(port, host, () =>
 //   res.send(initialNodes);
 // });
 
+app.post("/check", (req, res) => {
+  [nodeId] = req.body.text.trim();
+  nodesRef
+    .child(parseInt(nodeId))
+    .child("users")
+    .once("value")
+    .then(snapshot => {
+      if (snapshot.val() == null) {
+        res.send(`node${nodeId} is free!`);
+        return;
+      }
+      users = Object.keys(snapshot.val());
+      res.send(`node${nodeId} is taken by ${users.join(", ")}`);
+    });
+});
+
 app.post("/take", async (req, res) => {
   [nodeId, name] = req.body.text.trim().split(" ");
 
@@ -32,28 +48,49 @@ app.post("/take", async (req, res) => {
     .child(parseInt(nodeId))
     .child("users")
     .update({
-      [name]: ""
+      [name]: name
     });
 
-  res.send(`node${nodeId} was taken by ${name}`);
+  res.send(`node${nodeId} is now taken by ${name}`);
 });
 
-app.post("/checknode", async (req, res) => {
+app.post("/release", async (req, res) => {
   [nodeId, name] = req.body.text.trim().split(" ");
 
   await nodesRef
     .child(parseInt(nodeId))
     .child("users")
-    .update({
-      [name]: ""
-    });
+    .child(name)
+    .remove();
 
-  res.send(`node${nodeId} was taken by ${name}`);
+  res.send(`node${nodeId} is now released from ${name}`);
 });
 
-// available: 현재 사용하지 않는 노드 리스트 반환
-// check node##: node ##를 사용하고 있는 사용자 반환 (없으면 not occupied)
-// take node## {user}: node ##를 자신이 사용하도록 설정
+app.post("/findfreenodes", (req, res) => {
+  nodesRef.once("value").then(snapshot => {
+    nodes = snapshot.val();
+    freeNodes = [];
+
+    for (var id in nodes) {
+      console.log(nodes[id].users);
+      if (
+        nodes[id].users == undefined ||
+        Object.keys(nodes[id].users).length == 0
+      ) {
+        freeNodes.push("node" + id);
+      }
+    }
+
+    if (freeNodes.length == 0) {
+      res.send("Unfortunately, no nodes are available.");
+    } else {
+      res.send(
+        `Currently, ${freeNodes.join(", ")} are free. Take one of them!`
+      );
+    }
+  });
+});
+
 // release node## {user}: node ##를 더이상 자신이 사용안하는 것으로 설정
 
 // alive: 현재 사용 '가능한' 노드 리스트 반환

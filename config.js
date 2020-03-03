620536a0e867f8f58dc0d9b51648bb62fb7e0ff7
greenmon <greenmon@kaist.ac.kr>
2020-03-03 16:49:07 +0900

    Add config file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: process.env.API_KEY,
+  authDomain: "node-monitor-957e8.firebaseapp.com",
+  databaseURL: "https://node-monitor-957e8.firebaseio.com",
+  projectId: "node-monitor-957e8",
+  storageBucket: "node-monitor-957e8.appspot.com",
+  messagingSenderId: "491312307957",
+  appId: "1:491312307957:web:fbcb6ab09b43e13482afbc",
+  measurementId: "G-MMM283G7XQ"
+};
